c6e7c5a168b6647e8e01f5bc4846bce9fd586ace
Kaneko-Yukihiro0117 <74887355+Kaneko-Yukihiro0117@users.noreply.github.com>
2020-12-16 15:12:30 +0900

    ユーザー管理実装のコメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,22 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+ 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
     validates :nickname, length: {minimum: 3 }
-    # ニックネームの文字数を１文字や２文字にされないよう３文字以上の制約をつける
     validates :email,    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                          uniqueness: true
-    #  メールアドレスに一意性のあるバリテーションをつける、また@をを含む正しい形式で入力されている
     validates :password,              format: { with: /\A^(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,20}$\z/, message: 'は英字と数字両方を含むパスワードを設定してください' }
-     # 英字と数字の両方を含む6文字以上20文字以下
     validates :birthday
     with_options length: { maximum: 30 } do
       with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
         validates :first_name_kanji
         validates :last_name_kanji
-    # 全角漢字。平仮名。カタカナを取り入れる。また文字数無制限にされても困るので30文字で留める。
        end
       with_options format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ } do
         validates :first_name_kana
         validates :last_name_kana
-    # カタカナのみと設定、また文字数無制限にされても困るので30文字で留める。
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,6 @@ describe User do
       @user.nickname = nil
       @user.valid?
       expect(@user.errors.full_messages).to  include("Nickname can't be blank")
-      #ニックネームが必須 
     end
     it  "emailがない場合は登録できないこと" do
       @user.email = nil
@@ -46,42 +45,34 @@ describe User do
       @user.valid?
       expect(@user.errors.full_messages).to include("Last name kana can't be blank")
     end
-    #ユーザー本名が、名字と名前でそれぞれ必須 & ユーザー本名のフリガナが、名字と名前でそれぞれ必須
     it "birthdayがない場合は登録できないこと" do
       @user.birthday = nil
       @user.valid?
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
-    #生年月日が必須
     it "重複したemailが存在する場合登録できないこと" do
       @user.save
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
-      # 2人目のユーザー
       another_user.valid?
       expect(another_user.errors.full_messages).to  include("Email has already been taken")
     end
-    #メールアドレスは一意である
     it "emailの中間に@がない場合、登録できないこと" do
       @user.email = "email"
       @user.valid?
       expect(@user.errors.full_messages).to include("Email is invalid") 
     end
-    #メールアドレスは@とドメインを含む必要がある
     it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
       @user.password_confirmation = "" 
-      # 故意に確認用のパスワードを空にしてみる
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
-    #パスワードは確認用を含めて2回入力する
     it "passwordが6文字以下であれば登録できないこと" do
       @user.password = "123d5"
       @user.password_confirmation = "123d5"
       @user.valid?
       expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)", "Password は英字と数字両方を含むパスワードを設定してください")
     end
-    #パスワードは6文字以上
     it "passwordが数字のみの場合、登録できないこと" do
       @user.password = "1234567"
       @user.password_confirmation = "1234567"
@@ -102,13 +93,11 @@ describe User do
     end
     it 'first_name_kanjiが全角入力でなければ登録できないこと' do
       @user.first_name_kanji = "kana"
-      # 故意に半角入力にしてみる
       @user.valid?
       expect(@user.errors.full_messages).to include("First name kanji is invalid")
     end
     it 'last_name_kanjiが全角入力でなければ登録できないこと' do
       @user.last_name_kanji = "kana"
-      # 故意に半角入力にしてみる
       @user.valid?
       expect(@user.errors.full_messages).to include("Last name kanji is invalid")
     end
@@ -133,7 +122,6 @@ describe User do
       @user.valid?
       expect(@user.errors.full_messages).to include("Last name kana is invalid")
     end
-    #ユーザー本名のフリガナは全角で入力させる
       end
     end
   end
