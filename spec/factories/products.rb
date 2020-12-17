2dfb2c8878c169f570b49ff9efb8a2ca3c513214
Kaneko-Yukihiro0117 <74887355+Kaneko-Yukihiro0117@users.noreply.github.com>
2020-12-17 16:35:26 +0900

    商品出品のテストコード実装

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,4 +1,18 @@
 FactoryBot.define do
   factory :product do
+    name                 {"hugahuga"}
+    introduction         {Faker::Lorem.sentences}
+    price                {"9999998"}
+    category_id          {"2"}
+    item_condition_id    {"2"}
+    prefecture_id        {"2"}
+    delivery_fee_id      {"2"}
+    delivery_date_id     {"2"}
+
+    association :user
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/flag.png'), filename: 'flag.png')
+    end
   end
 end
