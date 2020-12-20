cbd790f049b6b52893aa2e76ed5a97d9898d1b81
Kaneko-Yukihiro0117 <74887355+Kaneko-Yukihiro0117@users.noreply.github.com>
2020-12-20 10:40:43 +0900

    商品編集の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: :show
+  before_action :set_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.all.order('created_at DESC')
@@ -18,21 +18,34 @@ class ProductsController < ApplicationController
     else
       render :new
     end
+  end
 
-    def show
+  def show
+  end
+
+  def edit
+    redirect_to root_path unless user_signed_in? && current_user.id == @product.user_id
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    if @product.update_attributes(product_params)
+      redirect_to root_path
+    else
+      render action: :edit
     end
   end
+end
 
   private
 
-  def product_params
-    params.require(:product).permit(
-      :name, :introduction, :price, :category_id, :delivery_fee_id, :delivery_date_id, :prefecture_id, :item_condition_id,
-      :image
-    ).merge(user_id: current_user.id)
-  end
+def product_params
+  params.require(:product).permit(
+    :name, :introduction, :price, :category_id, :delivery_fee_id, :delivery_date_id, :prefecture_id, :item_condition_id,
+    :image
+  ).merge(user_id: current_user.id)
+end
 
-  def set_product
-    @product = Product.find(params[:id])
-  end
+def set_product
+  @product = Product.find(params[:id])
 end
